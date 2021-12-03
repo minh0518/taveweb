4357abeeb1c2554481bf308daed501eaa7fdd536
gyeongminK <sally_k@naver.com>
2021-12-03 18:49:58 +0900

    [UPDATE] answers 라우터 수정

--- backend/routes/answers.js
+++ backend/routes/answers.js
@@ -4,38 +4,66 @@ const Answer = require('../models/answer');
 
 const router = express.Router();
 
-// router.get('/', async (req, res, next) => {
-//     try {
-//         const answers = await Answer.findAll({
-//             attributes: ['id', 'content', 'question_id'],
-//         });
-//         res.status(200).json({ answers });
-//     } catch (error) {
-//         console.error(err);
-//         next(err);
-//     }
-// });
+//디비 확인용 라우터
+router.get('/', async (req, res, next) => {
+    try {
+        const answers = await Answer.findAll({
+            attributes: ['id', 'content', 'question_id'],
+        });
+        res.status(200).json({ answers });
+    } catch (error) {
+        console.error(err);
+        next(err);
+    }
+});
 
-// router.post('/:id', async (req, res, next) => {
-//     try {
-//         console.log(req.params.id);
-//         const answer = await Answer.create({
-//             content: req.body.content,
-//             question_id: req.params.id,
-//         });
-//         console.log(answer);
-//         res.status(201).json(answer);
-//     } catch (err) {
-//         console.error(err);
-//         next(err);
-//     }
-// });
+// question_id랑 연결되어있기 때문에 question_id에 해당되는 질문이 없으면 오류남.
 
-// router.delete('/:id', async (req,res,next)=>{
-//     try{
-//         const
-//     }
-// })
+router
+    .route('/:id') //여기서 id는 question_id,,, 이렇게 해도 되나 모르겠음
+    .post(async (req, res, next) => {
+        try {
+            console.log(req.params.id);
+            const answer = await Answer.create({
+                content: req.body.content,
+                question_id: req.params.id,
+            });
+            //console.log(answer);
+            res.status(201).json(answer);
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
+    })
+
+    .patch(async (req, res, next) => {
+        try {
+            const answer = await Answer.update(
+                {
+                    content: req.body.content,
+                },
+                {
+                    where: { question_id: req.params.id },
+                }
+            );
+            res.json({ answer });
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
+    })
+
+    .delete(async (req, res, next) => {
+        try {
+            const result = await Answer.destroy({
+                where: { question_id: req.params.id },
+            });
+            res.json({ result });
+        } catch (err) {
+            console.error(err);
+            next(err);
+        }
+    });
 
 // router.post('/', async (req, res, next) => {
 //     try {
