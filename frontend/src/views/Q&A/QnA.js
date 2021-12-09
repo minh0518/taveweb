c00633fe2369f95853a9942148fa034f35fc8331
minh0518 <rubics@naver.com>
2021-12-09 23:45:23 +0900

    error

--- frontend/src/views/Q&A/QnA.js
+++ frontend/src/views/Q&A/QnA.js
@@ -3,24 +3,72 @@ import axios from 'axios';
 import { Route, Routes, Link } from 'react-router-dom';
 
 const QnA = () => {
-    
+    const [value, setValue] = useState([]);//질문들
+
+    const [value2, setValue2] = useState([]);//답변들
+
     useEffect(function(){
-        axios.get("/api/questions").then((result)=>{
-            console.log(result.data)
-        })
+        Questions()
     },[])
+
+    useEffect(function(){
+        Answers()
+    },[])
+
+    async function Questions(){
+        const response=await axios.get("/api/questions")
+        setValue(response.data.questions)
+    }
+    
+    async function Answers(){
+        const response=await axios.get("/api/answers")
+        setValue2(response.data.answers)
+    }
+
+  
+
+
+    
     return (
         <div>
-            <p>QnA 목록</p>
-        
-            
+        <p>QnA</p>
+    
+
+        {value.map((question) => ( // 질문들을 보여줌
+        <div>
+        <Link to={question.id}><p>{question.title}</p></Link> 
+        </div> 
+        ))}
+
+        {value.map((question) => (   //클릭하면 해당 question_id에 대한 답변 
+        <div>
+        <Routes>
+            <Route exact path={question.id}><DisplayAnswers answers={value2} questionId={question.id}></DisplayAnswers>
+            </Route>
+        </Routes> 
         </div>
+
+        ))}
+        
+    </div>
     );
 };
 
 
-function List(){
+const DisplayAnswers=function({answers,questionId}){
+    
+    answers.map(function(answer){
+
+        if(answer.question_id==={questionId}){
+            return(
+            <div>{answer.content}</div>
+            )
+        }
+        
+    })
+
     
 }
+
 
 export default QnA;
