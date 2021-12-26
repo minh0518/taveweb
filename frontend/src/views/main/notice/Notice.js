7b2526500b40901af4a557bfb1967a30579a8924
minh0518 <rubics@naver.com>
2021-12-26 12:45:02 +0900

    [ADD]Notices

--- frontend/src/views/main/notice/Notice.js
+++ frontend/src/views/main/notice/Notice.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 const Notice = () => {
+
+    const [value,setValue]=useState([]) //공지
+
+    useEffect(function(){
+        Notices()
+    },[])
+
+    async function Notices(){
+        const response=await axios.get("api/notices")
+        setValue(response.data.notices);
+    }
+
+
     return (
         <div>
-            <p>공지사항</p>
+            <h2>Notices</h2>
+           
         </div>
     );
 };
