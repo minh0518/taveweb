bc5e0e7d25f355c9b31db965d02208c1ec15348b
minh0518 <rubics@naver.com>
2022-01-09 23:12:24 +0900

    [UPDATE]footer위치조정

--- frontend/src/views/main/Footer.js
+++ frontend/src/views/main/Footer.js
@@ -7,13 +7,15 @@ const Info = styled.div`
     flex-direction: row;
 `;
 
+const Section = styled.div`
+    margin-left: 3%;
+`;
+
 const UnderLine = styled.hr`
     width: 80%;
     margin-left: 0px;
 `;
-const Section = styled.div`
-    margin-left: 3%;
-`;
+
 
 const Title = styled.p`
     font-size: xx-large;
